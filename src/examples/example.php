92e4be5a9e117a4b7df268f7a23df668d53b429c
shellus <shellus@endaosi.com>
2016-12-12 15:59:07 +0800

    批量采集成功，很棒很好用哦

--- src/examples/example.php
+++ src/examples/example.php
@@ -11,49 +11,49 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Promise;
 
 require __DIR__ . '/bootstrap.php';
 
 
 // 处理接收到的数据
-function handleResult(Response $response)
+function handleResult(Response $response, $index)
 {
-    $text = substr($response->getBody(), 0, 200);
-    $text = str_replace("\r\n", "\n", $text);
-    $text = str_replace("\n", "", $text);
-    var_dump("status: {$response -> getStatusCode()}  content: $text");
+    file_put_contents(__DIR__ . '/storage/' . $index . '.json', $response->getBody());
+    var_dump($index);
 }
 
-// 并发50
+// 处理接收到的数据
+function handleRequestException(RequestException $e)
+{
+    var_dump( $e->getMessage());
+    var_dump( $e->getRequest()->getMethod());
+}
+
 $client = new Client();
 
-for ($i = 0; $i < 10; $i++) {
+function build_request(){
+    $date = $start_date = "20160221";
+    $end_date = "20161101";
+    while(strtotime($date) < strtotime($end_date)){
 
-    $url = 'http://www.xjflcp.com/game/SelectDate';
-    $body = \GuzzleHttp\Psr7\build_query(['selectDate' => '20161122']);
-    $headers = [
-        "Host" => "www.xjflcp.com",
-        "Connection" => "keep-alive",
-        "Accept" => "text/plain, */*; q=0.01",
-        "Origin" => "http://www.xjflcp.com",
-        "X-Requested-With" => "XMLHttpRequest",
-        "User-Agent" => "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36",
-        "Content-Type" => "application/x-www-form-urlencoded; charset=UTF-8",
-        "Referer" => "http://www.xjflcp.com/game/sscAnnounce",
-        "Accept-Encoding" => "gzip, deflate",
-        "Accept-Language" => "zh-CN,zh;q=0.8",
-    ];
-    $request = new Request('POST', $url, $headers, $body);
+        $url = 'http://www.xjflcp.com/game/SelectDate';
+        $headers = [
+            "Content-Type" => "application/x-www-form-urlencoded; charset=UTF-8",
+        ];
+        $body = \GuzzleHttp\Psr7\build_query(['selectDate' => $date]);
+        $request = new Request('POST', $url, $headers, $body);
+        yield $date => $request;
 
-    $promise = $client->sendAsync($request);
+        $timestamp = strtotime("{$date} +1 days");
+        $date = date('Ymd', $timestamp);
+    }
 }
 
-$promise->then(
-    function ($response) {
-        handleResult($response);
-    },
-    function (RequestException $e) {
-        echo $e->getMessage() . "\n";
-        echo $e->getRequest()->getMethod();
-    });
-$promise->wait();
+
+$pool = new \GuzzleHttp\Pool($client, build_request(),[
+    'concurrency' => 30,
+    'fulfilled' => 'handleResult',
+    'rejected' => 'handleRequestException',
+]);
+$pool ->promise() -> wait();
